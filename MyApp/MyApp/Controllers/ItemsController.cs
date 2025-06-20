33d3736c196229b23969137df618dda4a2bf0c30
Gavindu Sajeendra <gavindu1995@gmail.com>
2025-06-20 08:39:33 +0530

    .NET core project added

---
+++ MyApp/MyApp/Controllers/ItemsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+namespace MyApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item() { Name = "keyboard"};
+            return View(item);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+    }
+}
